2446b67b729c8235ec50a500173080aa12ee0eec
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-02-13 15:57:04 +0100

    composant BarForm

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,21 @@
 <script setup>
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import { ref } from "vue";
+import BarFormComponent from "@/components/BarFormComponent.vue";
 
 const title = ref('Vous avez un besoin ?');
 const icons = ref([
   { name: 'mdi-menu', size: 'x-large', color: '#868686' },
   { name: 'mdi-account-circle', size: 'x-large', class: 'ml-3' }
 ]);
+
+const fields = ref([
+  { type: 'text', label: 'écrivez le titre du besoin', name: 'Quoi ?:', required: true },
+  { type: 'date', label: '../../../', name: 'Quand ?:', required: true },
+  { type: 'select', label: 'Compétences', name: 'Quelle que compétence :', items: ['Type 1', 'Type 2', 'Type 3'], required: true }
+]);
+
+const save = ref('Créer un besoins');
 
 const logo = ref('Crazy');
 </script>
@@ -25,6 +34,7 @@ const logo = ref('Crazy');
       </v-icon>
     </template>
   </HeaderComponent>
+  <BarFormComponent :fields="fields" :save="save" />
 </template>
 
 <style scoped>
