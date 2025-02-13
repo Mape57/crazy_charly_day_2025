99d05019267c7e985051c45f50f05f701c0d981d
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-02-13 14:43:32 +0100

    save formulaire changements

---
+++ frontend/src/components/FormComponent.vue
@@ -0,0 +1,107 @@
+<script setup>
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  fields: {
+    type: Array,
+    required: true
+  },
+  buttonText: {
+    type: String,
+    required: true
+  },
+});
+
+const handleSubmit = async () => {
+  try {
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit" class="form-container">
+    <h2>{{ title }}</h2>
+    <div class="input-container" v-for="field in fields" :key="field.name">
+      <label :for="field.name">{{ field.label }}</label>
+      <template v-if="field.type === 'rating'">
+        <v-rating :name="field.name" hover half-increments :length="5" :size="32" active-color="#45FF30"></v-rating>
+      </template>
+      <template v-else-if="field.options">
+        <select :name="field.name">
+          <option v-for="option in field.options" :key="option" :value="option">{{ option }}</option>
+        </select>
+      </template>
+      <template v-else>
+        <input :type="field.type" :name="field.name" :placeholder="field.placeholder">
+      </template>
+      <span class="divider"></span>
+    </div>
+    <button type="submit">{{ buttonText }}</button>
+  </form>
+</template>
+
+<style scoped>
+.form-container {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #868686;
+  border-radius: 40px;
+  padding: 40px;
+}
+
+h2 {
+  font-size: 24px;
+  font-family: 'Montserrat', sans-serif;
+  text-decoration: underline;
+  margin-bottom: 15px;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+}
+
+.input-container label {
+  font-size: 20px;
+  font-family: 'Montserrat', sans-serif;
+  margin-bottom: 5px;
+  color: #000000;
+  font-weight: bold;
+}
+
+.input-container input {
+  padding: 10px;
+  border-radius: 10px;
+  border: 1px solid #868686;
+  font-size: 16px;
+}
+
+.divider {
+  height: 1px;
+  background-color: #D9D9D9;
+  margin: 20px 0;
+}
+
+button {
+  background-color: #45FF30;
+  color: #1E1E1E;
+  padding: 14px 20px;
+  margin: 8px 0;
+  cursor: pointer;
+  width: 100%;
+  border: 1px solid black;
+  font-size: 24px;
+  border-radius: 25px;
+  font-weight: bold;
+}
+
+button:hover {
+}
+</style>
--- frontend/src/views/SalarieView.vue
+++ frontend/src/views/SalarieView.vue
@@ -2,12 +2,40 @@
 import {useUserStore} from "@/stores/userStore.js";
 import {onMounted, ref} from "vue";
 import axios from "axios";
+import FormComponent from "@/components/FormComponent.vue";
 
 const userStore = useUserStore();
 
 const user = ref({
   name: '',
-  competences: [],
+  competences: [
+    {
+      id: 1,
+      name: 'Bricolage',
+      libelle: 'Lorem ipsum dolor sit amet consectetur. Elementum convallis a malesuada tincidunt. Nunc ac aliquam quis quam dui velit. Donec sollicitudin cursus tristique hac sagittis eu vitae. Malesuada nisi dui ut arcu faucibus tristique platea gravida.'
+    },
+    {
+      id: 2,
+      name: 'Cuisine',
+      libelle: 'Lorem ipsum dolor sit amet consectetur. Elementum convallis a malesuada tincidunt. Nunc ac aliquam quis quam dui velit. Donec sollicitudin cursus tristique hac sagittis eu vitae. Malesuada nisi dui ut arcu faucibus tristique platea gravida.'
+    },
+    {
+      id: 3,
+      name: 'Jardinage',
+      libelle: 'Lorem ipsum dolor sit amet consectetur. Elementum convallis a malesuada tincidunt. Nunc ac aliquam quis quam dui velit. Donec sollicitudin cursus tristique hac sagittis eu vitae. Malesuada nisi dui ut arcu faucibus tristique platea gravida.'
+    },
+    {
+      id: 4,
+      name: 'Informatique',
+      libelle: 'Lorem ipsum dolor sit amet consectetur. Elementum convallis a malesuada tincidunt. Nunc ac aliquam quis quam dui velit. Donec sollicitudin cursus tristique hac sagittis eu vitae. Malesuada nisi dui ut arcu faucibus tristique platea gravida.'
+    },
+  ],
+  userCompetences: [
+    'Bricolage',
+    'Cuisine',
+    'Jardinage',
+    'Informatique',
+  ],
 });
 
 async function getCompetencesBySalarieId(id) {
@@ -17,8 +45,19 @@ async function getCompetencesBySalarieId(id) {
         Authorization: `Bearer ${userStore.token}`,
       },
     }).then((res) => {
-      return res.data;
+      user.value.competences = res.data;
     });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getCompetences() {
+  try {
+    const response = await axios.get('http://localhost:8080/competences')
+        .then((res) => {
+          user.value.userCompetences = res.data;
+        });
   } catch (error) {
     console.error(error);
   }
@@ -26,6 +65,7 @@ async function getCompetencesBySalarieId(id) {
 
 onMounted(async () => {
   user.value.name = userStore.name;
+  await getCompetences();
   await getCompetencesBySalarieId(userStore.id);
 });
 </script>
@@ -42,13 +82,24 @@ onMounted(async () => {
         <p>Compétences:</p>
         <v-rating half-increments readonly :length="5" :size="32" :model-value="3" active-color="green"></v-rating>
       </div>
+      <FormComponent
+          title="Nouvelle compétence ?"
+          button-text="Demander ma compétence"
+          :fields="[
+            { label: 'Quoi?', type: 'text', name: 'nom', placeholder: 'Quoi de neuf', options: user.userCompetences },
+            { label: 'Vraiment?', type: 'rating', name: 'rating' },
+          ]"
+      />
     </div>
     <div class="divider"></div>
     <div class="right-side">
       <div>
         <h2>Dernières missions:</h2>
         <ul>
-          <li v-for="competence in user.competences" :key="competence.id">{{ competence.name }}</li>
+          <li v-for="competence in user.competences" :key="competence.id">{{ competence.name }}{{
+              competence.libelle
+            }}
+          </li>
         </ul>
       </div>
     </div>
@@ -58,19 +109,34 @@ onMounted(async () => {
 <style scoped>
 header {
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: flex-start;
+  width: 30%;
+  border-right: 3px solid #868686;
+  height: 80px;
+  padding: 0 20px;
 }
 
 header h1 {
   font-family: 'Montserrat', sans-serif;
   font-weight: normal;
   font-size: 36px;
+  height: 100%;
 }
 
-.divider {
-  width: 1px;
-  height: 100%;
-  background-color: #000;
+main {
+  display: flex;
+  align-items: flex-start;
+}
+
+.left-side {
+  width: 30%;
+  padding: 0 50px;
+  border-right: 3px solid #868686;
+}
+
+.right-side {
+  width: 70%;
+  padding-left: 50px;
 }
 </style>
