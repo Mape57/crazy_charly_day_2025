cc5aef6735956bd1d673bc2e410a3aff469a507d
Thomas M <micheli17u@etu.univ-lorraine.fr>
2025-02-13 13:04:57 +0100

    salaries fini

--- serveur/src/main/java/charlyday/ccd/utilisateur/UtilisateurMapper.java
+++ serveur/src/main/java/charlyday/ccd/utilisateur/UtilisateurMapper.java
@@ -1,10 +1,12 @@
 package charlyday.ccd.utilisateur;
 
 import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
+@Mapper
 public interface UtilisateurMapper {
     UtilisateurMapper INSTANCE = Mappers.getMapper(UtilisateurMapper.class);
 
