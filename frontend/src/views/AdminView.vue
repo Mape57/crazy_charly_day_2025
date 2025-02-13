671e6400a482ffae5fbb21a31cea1f1a29dcfd05
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2025-02-13 21:33:33 +0100

    Implement data fetching for clients, salaries, and needs in AdminView component

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -3,7 +3,6 @@ import HeaderComponent from "@/components/HeaderComponent.vue";
 import { ref } from "vue";
 import BarFormComponent from "@/components/BarFormComponent.vue";
 import SelectedComponent from "@/components/SelectedComponent.vue";
-import CardComponent from "@/components/CardComponent.vue";
 import axios from "axios";
 
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
@@ -11,10 +10,50 @@ const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 const title = ref('Vous avez un besoin ?');
 const icons = ref('test');
 
+const getSalaries = async () => {
+  try {
+    const response = await axios.get(`${apiBaseUrl}/salaries`);
+    response.data = response.data.map((item) => {
+      return item.nom;
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+const getClients = async () => {
+  try {
+    const response = await axios.get(`${apiBaseUrl}/clients`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+const getBesoins = async () => {
+  try {
+    const response = await axios.get(`${apiBaseUrl}/besoins`);
+    response.data = response.data.map((item) => {
+      return item.description;
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+const salarieItems = ref([]);
+const clientItems = ref([]);
+const besoinItems = ref([]);
+
 const fields = ref([
-  { type: 'select', label: 'sélectionner un client', name: 'Qui ?:', items: [], required: true },
-  { type: 'select', label: 'sélectionner un besoin', name: 'Quoi ?:',items: [], required: true },
-  { type: 'select', label: 'sélectionner un  salarié', name: 'Avec Qui :', items: [], required: true }
+  { type: 'select', label: 'sélectionner un client', name: 'Qui ?:', items: clientItems, required: true },
+  { type: 'select', label: 'sélectionner un besoin', name: 'Quoi ?:', items: besoinItems, required: true },
+  { type: 'select', label: 'sélectionner un salarié', name: 'Avec Qui :', items: salarieItems, required: true }
 ]);
 
 const items = ref([
@@ -25,22 +64,22 @@ const items = ref([
 ]);
 
 const save = ref('Affecter');
-
 const logo = ref('Crazy');
 
-const getBesoins = async () => {
+const fetchData = async () => {
   try {
+    salarieItems.value = await getSalaries();
+    clientItems.value = await getClients();
+    besoinItems.value = await getBesoins();
     const response = await axios.get(`${apiBaseUrl}/besoins`);
     items.value = response.data;
-    console.log(items.value);
     console.log(response);
   } catch (error) {
     console.error(error);
   }
-}
+};
 
-
-getBesoins();
+fetchData();
 </script>
 
 <template>
