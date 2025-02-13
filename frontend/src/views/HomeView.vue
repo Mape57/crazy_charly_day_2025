d6bb18de083a2bb5a626b1f07ca0c637f1cd0d56
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-02-13 14:02:43 +0100

    composant header

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import { ref } from "vue";
+
+const title = ref('Vous avez un besoin ?');
+const icons = ref([
+  { name: 'mdi-menu', size: 'x-large', color: '#868686' },
+  { name: 'mdi-account-circle', size: 'x-large', class: 'ml-3' }
+]);
+
+const logo = ref('Crazy');
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <HeaderComponent :title="title" :logo="logo">
+    <template #icons>
+      <v-icon
+          v-for="(icon, index) in icons"
+          :key="index"
+          :size="icon.size"
+          :color="icon.color"
+          :class="icon.class"
+      >
+        {{ icon.name }}
+      </v-icon>
+    </template>
+  </HeaderComponent>
 </template>
+
+<style scoped>
+</style>
