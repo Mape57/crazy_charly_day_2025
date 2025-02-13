b5ea204e2a58ff4779bc3943468852b6e3eb38d0
Achraf-ops <achraf.raouf1@etu.univ-lorraine.fr>
2025-02-13 17:25:40 +0100

    Besoins?page=1&limite=10

--- serveur/src/main/java/charlyday/ccd/besoins/BesoinsService.java
+++ serveur/src/main/java/charlyday/ccd/besoins/BesoinsService.java
@@ -3,6 +3,8 @@ package charlyday.ccd.besoins;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.UUID;
@@ -22,6 +24,10 @@ public class BesoinsService {
 		return besoinsRepository.findAll();
 	}
 
+	public Page<BesoinsEntity> getAllBesoinsPaginated(Pageable pageable) {
+		return besoinsRepository.findAll(pageable);
+	}
+
 	public BesoinsEntity createBesoins(BesoinsEntity entity){return besoinsRepository.save(entity);}
 
 	public BesoinsEntity updateBesoins(BesoinsEntity entity){return besoinsRepository.save(entity);}
@@ -31,4 +37,6 @@ public class BesoinsService {
 	public BesoinsEntity getBesoinsById(UUID id) {
 		return besoinsRepository.findById(id).orElse(null);
 	}
+
+
 }
